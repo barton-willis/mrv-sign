486612442a79441e8ca0615d38d8dd9f6d8d4728
Barton Willis <58446958+barton-willis@users.noreply.github.com>
2024-01-15 18:38:41 -0600

    Create mrv-sign.lisp

---
+++ mrv-sign.lisp
@@ -0,0 +1,127 @@
+(defvar *mrv-sign* nil)
+
+
+;; Do {neg, zero, pos} --> {-1,0,1}. For all other inputs, return nil
+(defun mrv-sign-to-number (sgn)
+	(cond ((eq sgn '$neg) -1)
+		  ((eq sgn '$zero) 0)
+		  ((eq sgn '$pos) 1)
+		  (t nil))) ; pn, pz, nz,pnz, complex, imaginary.
+
+(defun mrv-sign-mabs (e x)
+	(let ((sgn (mrv-sign-helper (cadr e) x)))
+		(cond ((eql sgn -2) 2) ;abs(minf) = inf
+		      ((eql sgn 2) 2) ;abs(inf) = inf
+			  ((eql sgn 1) 1) ;abs(pos) = pos
+			  ((eql sgn -1) 1) ;abs(neg) = inf
+			  ((eq t (mnqp 0 (cadr e))) 1) ; abs(nonzero) = pos
+			  (t (mrv-sign-to-number ($csign e))))))
+			    
+(defun mrv-sign-constant (e x)
+	(let ((sgn))
+	  (cond ((eq e '$minf) -2)
+	        ((eq e '$zerob) -1)
+	  	    ((eq e '$zeroa) 1)
+		    ((eq e '$inf) 2)
+		    ((or (eq e '$infinity) (eq e '$ind)  (eq e '$und)) nil)
+		    (t 
+		      (setq sgn ($csign ($radcan e))) ;not sure about the radcan
+			  ;; Do an asksign when sgn = pnz & e is freeof x
+		  	  (when (and (or t *getsignl-asksign-ok*) (eq sgn '$pnz) (freeof x e))
+				(setq sgn ($asksign e)))
+			  (mrv-sign-to-number sgn)))))
+
+(defun mrv-sign-sum (e x)
+	(let ((ee (mapcar #'(lambda (q) (mrv-sign-helper q x)) (cdr e))))
+	(cond 
+	    ;; unable to find the sign of one term, dispatch csign on e.
+		((some #'null ee)
+		  (mrv-sign-to-number ($csign e)))
+	    ;; every term of e is nonnegative, return 1 for postive & 2 for inf
+	    ((every #'(lambda (q) (>= q 0)) ee) (apply #'max ee))
+		;; every term is nonpositive, return -1 for negative & -2 for minf
+		((every #'(lambda (q) (>= 0 q)) ee) (apply #'min ee))
+		((and (every #'(lambda (q) (>= q -1)) ee) (member 2 ee :test #'eql))
+		 2)
+		
+		
+		;; see if csign can do it
+	    (t (mrv-sign-to-number ($csign e))))))
+ 
+(defun mrv-sign-product (e x)
+  (let ((ee (mapcar #'(lambda (q) (mrv-sign-helper q x)) (cdr e))))
+  (mtell "e = ~M ~%" (cons '(mlist) ee))
+  (cond
+     ;; unable to find the sign of one term, dispatch csign on e.
+	 ((some #'null ee)
+		  (mrv-sign-to-number ($csign e)))
+     ;; one or more zero terms and one or more infinite terms, dispatch csign on e 
+     ((and (member 0 ee) (or (member -2 ee) (member 2 ee))) ;indeterminate
+	      (mrv-sign-to-number ($csign e)))
+     (t  (max -2 (min 2 (reduce '* ee)))))))
+
+(defun mrv-sign-log (e x) ; e = log(X)
+	(let ((sgn (mrv-sign-helper (cadr e) x)))
+      (cond 
+		;; log(inf) = inf	 
+	    ((eql sgn 2) 2)
+		;; for all other cases, dispatch csign
+		(t (mrv-sign-to-number ($csign e))))))
+
+(defun mvr-sign-mexpt (e x) ; e = X^Y
+  (let ((a (mrv-sign-helper (cadr e) x))
+        (b (mrv-sign-helper (caddr e) x)))
+	(cond 
+	  ;; pos^minf = pos
+	  ((and (eql a 1) (eql b -2)) 1)
+	  ;; pos^inf = inf
+	  ((and (eql a 1) (eql b 2)) 2)
+	  ;; inf^pos = inf & inf^inf = inf
+	  ((and (eql a 2) (or (eql b 1) (eql b 2))) 2)
+      ;; inf^{neg, zero} = pos
+	  ((and (eql a 2) (or (eql b -1) (eql b 0))) 1)
+	  ;; pos^{neg, zero, pos} = pos
+	  ((and (eql a 1) (or (eql b -1) (eql b 0) (eql b 1))) 1)
+	  ;; for all other cases, dispatch csign
+	  (t (mrv-sign-to-number ($csign e))))))
+
+;; True iff e = abs(X)
+(defun mabsp (e)
+	(and (consp e) (eq (caar e) 'mabs)))
+
+(defvar *missing-mrv* nil)
+(defun mrv-sign-helper (e x)
+	(cond ((freeof x e) (mrv-sign-constant e x))
+	      ((eq e x) 2)
+		  ((alike e (mul -1 x)) -2)
+	      ((mtimesp e) (mrv-sign-product e x))
+		  ((mplusp e) (mrv-sign-sum e x))
+		  ((mexptp e) (mvr-sign-mexpt e x))
+		  ((mlogp e) (mrv-sign-log e x))
+		  ((mabsp e) (mrv-sign-mabs e x))
+		  (t 
+		    (when (consp e)
+				(push (caar e) *missing-mrv*))
+		  
+		    (mrv-sign-to-number ($csign e)))))
+
+(defun $larry (e x)
+	(mrv-sign e x))
+
+;; bugs 
+;; larry(-log(x),x) = -1 (should be -2)
+;; larry(1-log(x),x) = false (should be -2)
+;; larry((1-log(x))*log(x),x) = false (should be -2)
+(defun mrv-sign (e x)
+	(let ((sgn (mrv-sign-helper e x)))
+	  (when (alike1 e (mul -1 (ftake '%log x)))
+	  	(setq sgn -2))
+	  (when (alike1 e (sub 1 (ftake '%log x)))
+	  	(setq sgn -2))
+     (when (alike1 e (mul (sub 1 (ftake '%log x)) (ftake '%log x)))
+	  	(setq sgn -2))
+	  (when (and (null sgn) (eq '$pnz ($csign e)))
+	    (push (ftake 'mlist e x sgn ($csign e)) *mrv-sign*)
+	    (mtell "Enter sign (either -1,0,or 1) of ~M ~%" e)
+	    (setq sgn ($read )))
+	 (if (null sgn) sgn (max -1 (min 1 sgn)))))
