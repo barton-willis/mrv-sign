36d33f12e07ea259c4d459452350e13a0f1cfcc6
Barton Willis <58446958+barton-willis@users.noreply.github.com>
2024-01-19 12:33:13 -0600

    update list of failed tests

--- mrv-sign.lisp
+++ mrv-sign.lisp
@@ -19,6 +19,8 @@
 ;; For debugging, the global *mrv-sign-failures* is a list of expressions that 
 ;; mrv-sign was not able to do. 
 
+;; rtest_gruntz failures:
+;(7 8 16 17 20 21 24 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 81 82 83 84 85 86 87 89 90 91 95 101 102)
 (defvar *mrv-sign-failures* nil)
 
 ;; Do {neg, zero, pos} --> {-1,0,1}. For all other inputs, throw an error
